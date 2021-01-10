84bd764b80a6affad7edeea05d6192ee4e2344f0
MarieNapravnik <napravnik.m@gmail.com>
2021-01-10 15:21:01 +0100

    My weather app in react
    From scratch, cleaning, start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import Weather from "./Weather";
+import React from "react";
+
 import './App.css';
 
-
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-    </div>
-  );
+  return <div className="App">My weather app in react</div>;
 }
+
