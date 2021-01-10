d812b05ec111f3c047aba97ecf6b035961a0e922
MarieNapravnik <napravnik.m@gmail.com>
2021-01-10 16:22:12 +0100

    Bootstrap added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 export default function App() {
   return <div className="App">
-    My weather app in react
+    <div className="container">
+   <h1> My weather app in react </h1>
+
     <footer>
       Coded by Marie Nápravníková and is
       <a href="https://github.com/MarieNapravnik/weather-react"
       rel="noreferrer"
       target="_blank"> open-sourced on GitHub</a>
     </footer>
+    </div>
     </div>;
 }
 
