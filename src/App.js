7484121f4a35e7499c23182efd0c4b04813e9c8c
MarieNapravnik <napravnik.m@gmail.com>
2021-01-10 15:35:03 +0100

    Open source added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">My weather app in react</div>;
+  return <div className="App">
+    My weather app in react
+    <footer>
+      Coded by Marie Nápravníková and is
+      <a href="https://github.com/MarieNapravnik/weather-react"
+      target="_blank"> open-sourced on GitHub</a>
+    </footer>
+    </div>;
 }
 
