c7769bcd52254014c4f41dd83153d88116476e79
MarieNapravnik <napravnik.m@gmail.com>
2021-01-17 11:53:01 +0100

    FormattedDate created
    default city added
    date created

--- src/App.js
+++ src/App.js
@@ -6,13 +6,22 @@ export default function App() {
   return <div className="App">
     <div className="container">
    <h1> My weather app in react </h1>
-<Weather />
+<Weather defaultCity="Brussels"/>
 
     <footer>
       Coded by Marie Nápravníková and is
       <a href="https://github.com/MarieNapravnik/weather-react"
       rel="noreferrer"
-      target="_blank"> open-sourced on GitHub</a>
+      target="_blank"> open-sourced on GitHub 
+      </a>{" "}
+      and{" "}
+          <a
+            href="https://awesome-leakey-a19f7a.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
     </footer>
     </div>
     </div>;
